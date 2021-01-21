045998ca2a05826d32757fae7f9860d23241408b
hayleyvuylsteke <72625026+hayleyvuylsteke@users.noreply.github.com>
2021-01-21 11:20:44 -0500

    bug fixes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,89 +1,73 @@
 var $todaysDate = $('#currentDay');
-var $timeBlockHour = $('.row');
-var $timeBlockEntry = $(".text-area");
-
-//Grab today's date and hour
-var currentDate = moment().format("dddd, MMMM DD, YYYY")
+var $timeBlockHour = $(".row");
+var $timeBlockEntry = $(".saveBtn");
+//Grab today’s date and hour
+var currentDate = moment().format("dddd, MMMM DD, YYYY");
 var currentHour = moment().format("H");
-
 var agendaItems = [""];
-
-
 //colour-coding time blocks based on time
-
 function initiateDay() {
-
-    $timeBlockHour.each(function() {
+    $timeBlockHour.each(function () {
         var $currentBlock = $(this);
         var currentBlockTime = parseInt($currentBlock.attr("data-hour"));
-
         var agendaItemObj = {
             hour: currentBlockTime,
             text: "",
-        }
-
+        };
+        // console.log(agendaItems);
         agendaItems.push(agendaItemObj);
     });
     //save data to storage
     localStorage.setItem("agendaItems", JSON.stringify(agendaItems));
 }
-
 function colorBlocking() {
     $timeBlockHour.each(function () {
         var $currentBlock = $(this);
         var currentBlockTime = parseInt($currentBlock.attr("data-hour"));
-
         if (currentBlockTime > currentHour) {
-            $currentBlock.addClass("future").removeClass("past present")
+            $currentBlock.addClass("future").removeClass("past present");
         } else if (currentBlockTime < currentHour) {
-            $currentBlock.addClass("past").removeClass("future present")
+            $currentBlock.addClass("past").removeClass("future present");
         } else {
-            $currentBlock.addClass("present").removeClass("future past") 
+            $currentBlock.addClass("present").removeClass("future past");
         }
-
     });
 }
-
-
-function buildSchedule(){
+function buildSchedule() {
     agendaItems = localStorage.getItem("agendaItems");
     agendaItems = JSON.parse(agendaItems);
 
-    for (var h = 0; h < agendaItems.length; h++){
+    for (var h = 0; h < agendaItems.length; h++) {
         var agendaHour = agendaItems[h].hour;
         var agendaText = agendaItems[h].text;
 
-        $("[data-hour" + agendaHour + "]").children("textarea").val(agendaText);
+        $(`.row[data-hour=${agendaHour}]`).children("textarea").val(agendaText);
     }
-
 }
-
 function saveButtonHandler() {
     var $currentBlock = $(this).parent();
+    console.log($currentBlock);
+    var updatedTime = $(this).parent().attr("data-hour");
+    var updatedItem = $(this).siblings("textarea").val();
 
-    var updatedTime = $(this).parent().attr("time-sig");
-    var updatedItem = (($(this).parent()).children("textarea")).val();
 
-    for (var h = 0; i < agendaItems.length; h++) {
-        if(agendaItems(i).hour == updatedTime) {
+    for (var h = 0; h < agendaItems.length; h++) {
+        if (agendaItems[h].hour == updatedTime) {
             agendaItems[h].text = updatedItem;
         }
     }
-
     localStorage.setItem("agendaItems", JSON.stringify(agendaItems));
-    buildSchedule();
+    // buildSchedule();
 }
-
-window.onload = function(){
+window.onload = function () {
     $todaysDate.text("Today is: " + currentDate);
-    
-    buildSchedule();
     colorBlocking();
-
+  
     if (!localStorage.getItem("agendaItems")) {
         initiateDay();
+    } else {
+        buildSchedule();
     }
-    $timeBlockEntry.on("click", "button", saveButtonHandler);
-}
-
-
+    // console.log(‘$timeBlockEntry:  ’, $timeBlockEntry);
+    $timeBlockEntry.on("click", saveButtonHandler);
+};
